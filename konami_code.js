75916f6bf92a2540cb19264152e57300ae4f623a
Vedrana <vedrana_ravlic16@yahoo.com>
2018-02-10 11:40:33 +0000

    automatically backed up by learn

--- konami_code.js
+++ konami_code.js
@@ -1,20 +1,23 @@
-const code = [38, 38, 40, 40, 37, 39, 37, 39, 66, 65];
-  let index = 0;
+const code = [38, 38, 40, 40, 37, 39, 37, 39, 66, 65]
 
-function init(){
-  document.body.addEventListener("keydown", function(e){
-    const key = parseInt(e.detail || e.which);
-    if(key===code[index]){
-      index++;
-        if(index===(code.length-1)){
-          window.alert("Hurray!");
-          index=0;
-        }
-        else{
-          index=0;
-        }
+function init() {
+  // Write your JavaScript code inside the init() function
+   document.body.addEventListener('keydown',function(e){
+      var key = parseInt(e.which || e.detail);
+      var index=0;
+      if(key === code[index]){
+      index++
+      
+     if(index === code.length){
+       alert("Hurray");
+       index=0;
      }
   }
-)}
+     else{
+       console.log(e.which);
+       index=0;
+     }
+})
+}
 
 init();
