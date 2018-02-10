94baa52144fb2f916c1716aca12f3718d9d07448
Vedrana <vedrana_ravlic16@yahoo.com>
2018-02-10 11:27:54 +0000

    automatically backed up by learn

--- konami_code.js
+++ konami_code.js
@@ -2,8 +2,8 @@ const code = [38, 38, 40, 40, 37, 39, 37, 39, 66, 65];
   let index = 0;
 
 function init(){
-  document.body.addEventListener("click", function(e){
-  const key = parseInt(code.detail || code.which);
+  document.body.addEventListener("keydown", function(e){
+  const key = parseInt(e.detail || e.which);
   if(key===code[index]){
     index++;
     if(index===code.length){
